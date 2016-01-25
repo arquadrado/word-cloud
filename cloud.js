088f5670944a0e4f0917e1ca435977591b9e7cef
archaos <antoniorquadrado@gmail.com>
2016-01-25 22:23:31 +0000

    word cloud first commit

---
+++ cloud.js
@@ -0,0 +1,38 @@
+var fill = d3.scale.category20();
+
+var width = 750,
+    height = 500;
+
+d3.layout.cloud()
+    .size([width, height])
+    .words([
+      "Hello", "world", "normally", "you", "want", "more", "words",
+      "than", "this"].map(function(d) {
+      return {text: d, size: 10 + Math.random() * 90, test: "haha"};
+    }))
+    .padding(0)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw)
+    .start();
+
+
+function draw(words) {
+  d3.select("#word-cloud").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width/2 + "," + height/2 + ")")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+}
